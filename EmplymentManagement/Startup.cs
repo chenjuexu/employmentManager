e232245170d03cb07d66cdf633aa7843c60fab61
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-26 16:17:44 -0400

    replace app.UseMvcWithDefaultRoute() with app.UseMvc
    all functionality keep the same with 32.1

--- EmplymentManagement/Startup.cs
+++ EmplymentManagement/Startup.cs
@@ -22,7 +22,7 @@ namespace EmplymentManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddXmlSerializerFormatters();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
@@ -36,7 +36,11 @@ namespace EmplymentManagement
 
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
