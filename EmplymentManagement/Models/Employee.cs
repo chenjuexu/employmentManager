741c9523d388998af99e6c0af0265dbe27884619
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-27 20:24:48 -0400

    form validation
    open http://localhost:5000/home/create

--- EmplymentManagement/Models/Employee.cs
+++ EmplymentManagement/Models/Employee.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EmplymentManagement.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,10 @@ namespace EmplymentManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
-        public string Department { get; set; }
+        public Dept Department { get; set; }
     }
 }
