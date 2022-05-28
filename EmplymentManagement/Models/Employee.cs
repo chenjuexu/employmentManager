e28cde8dc39a98396c66a153e93846f4ddc25de1
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-27 20:36:42 -0400

    using mutil-methods

--- EmplymentManagement/Models/Employee.cs
+++ EmplymentManagement/Models/Employee.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EmplymentManagement.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,13 @@ namespace EmplymentManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required, MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Display(Name = "Office Email")]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
-        public string Department { get; set; }
+        public Dept Department { get; set; }
     }
 }
