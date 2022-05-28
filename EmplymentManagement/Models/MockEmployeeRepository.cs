d2882074b303034988a60a0e4b30ed0d9baff459
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-27 21:34:25 -0400

    AddSingleton() -
    AddSingleton() - As the name implies, AddSingleton() method creates a Singleton service. A Singleton service is created when it is first requested. This same instance is then used by all the subsequent requests. So in general, a Singleton service is created only one time per application and that single instance is used throughout the application life time.

--- EmplymentManagement/Models/MockEmployeeRepository.cs
+++ EmplymentManagement/Models/MockEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmplymentManagement.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,10 +14,22 @@ namespace EmplymentManagement.Models
         {
             _employeeList = new List<Employee>()
         {
-            new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragimtech.com" },
-            new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@pragimtech.com" },
-            new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@pragimtech.com" },
+            new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@pragimtech.com" },
+            new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@pragimtech.com" },
+            new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@pragimtech.com" },
         };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
         }
 
         public Employee GetEmployee(int Id)
