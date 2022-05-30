015fc473d28787d25f684962ab83746be399e2c4
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-30 13:34:37 -0400

    add more  user

---
+++ EmplymentManagement/Models/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EmplymentManagement.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmplymentManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.IT,
+                    Email = "mary@pragimtech.com"
+                }, new Employee
+                {
+                    Id = 2,
+                    Name = "John",
+                    Department = Dept.IT,
+                    Email = "john@pragimtech.com"
+                }
+                );
+
+
+        }
+    }
+}
