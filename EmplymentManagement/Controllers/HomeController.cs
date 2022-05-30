e72a4487f2dd9299e5f55d134ebdfa5ff7ed61e5
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-05-30 15:23:16 -0400

    HomeContoller(action part)

--- EmplymentManagement/Controllers/HomeController.cs
+++ EmplymentManagement/Controllers/HomeController.cs
@@ -1,56 +1,70 @@
 ﻿using EmplymentManagement.Models;
 using EmplymentManagement.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmplymentManagement.Controllers
+
+namespace EmployeeManagement.Controllers
 {
-    
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,
+                              IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
-        
-        public ViewResult Index()
-        {
-            // retrieve all the employees
-            var model = _employeeRepository.GetAllEmployees();
-            // Pass the list of employees to the view
-            return View(model);
-        }
-         
-    // ? makes id method parameter nullable
-    public ViewResult Details(int? id)
-    {
-        HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
-        {
-            // If "id" is null use 1, else use the value passed from the route
-            Employee = _employeeRepository.GetEmployee(id ?? 1),
-            PageTitle = "Employee Details"
-        };
-
-        return View(homeDetailsViewModel);
-    }
         [HttpGet]
         public ViewResult Create()
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
-               // return RedirectToAction("details", new { id = newEmployee.Id });
+                string uniqueFileName = null;
+
+                // If the Photo property on the incoming model object is not null, then the user
+                // has selected an image to upload.
+                if (model.Photo != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot
+                    // To get the path of the wwwroot folder we are using the inject
+                    // HostingEnvironment service provided by ASP.NET Core
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    // To make sure the file name is unique we are appending a new
+                    // GUID value and and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    // Use CopyTo() method provided by IFormFile interface to
+                    // copy the file to wwwroot/images folder
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    // Store the file name in PhotoPath property of the employee object
+                    // which gets saved to the Employees database table
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
 
             return View();
