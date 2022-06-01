0d6dd5e0bd35f108c21e3ccfdeae1e15a560a2b3
chenjuexu <53376000+chenjuexu@users.noreply.github.com>
2022-06-01 16:17:52 -0400

    Log generated
    open http://localhost:15410/home/details/2
    in logfile or output window:
    2022-06-01 16:02:16.0404|TRACE|EmplymentManagement.Models.SQLEmployeeRepository|Trace Log
    2022-06-01 16:02:16.0404|DEBUG|EmplymentManagement.Models.SQLEmployeeRepository|Debug Log
    2022-06-01 16:02:16.0404|INFO|EmplymentManagement.Models.SQLEmployeeRepository|Information Log
    2022-06-01 16:02:16.0553|WARN|EmplymentManagement.Models.SQLEmployeeRepository|Warning Log
    2022-06-01 16:02:16.0553|ERROR|EmplymentManagement.Models.SQLEmployeeRepository|Error Log
    2022-06-01 16:02:16.0553|FATAL|EmplymentManagement.Models.SQLEmployeeRepository|Critical Log

--- EmplymentManagement/Models/SQLEmployeeRepository.cs
+++ EmplymentManagement/Models/SQLEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace EmplymentManagement.Models
     public class SQLEmployeeRepository : IEmployeeRepository
     {
         private readonly AppDbContext context;
+        private readonly ILogger<SQLEmployeeRepository> logger;
 
-        public SQLEmployeeRepository(AppDbContext context)
+        public SQLEmployeeRepository(AppDbContext context,ILogger<SQLEmployeeRepository> logger)
         {
             this.context = context;
+            this.logger = logger;
         }
 
         public Employee Add(Employee employee)
@@ -35,6 +38,13 @@ namespace EmplymentManagement.Models
         
         public IEnumerable<Employee> GetAllEmployees()
         {
+
+            logger.LogTrace("Trace Log");
+            logger.LogDebug("Debug Log");
+            logger.LogInformation("Information Log");
+            logger.LogWarning("Warning Log");
+            logger.LogError("Error Log");
+            logger.LogCritical("Critical Log");
             return context.Employees;
         }
 
